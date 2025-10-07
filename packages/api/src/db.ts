1e4af314a3fd2f885b2b558b6ddda86138319a19
zhanyysh <tynybekovjanyshbek@gmail.com>
2025-10-07 12:59:09 +0600

    port added

--- packages/api/src/db.ts
+++ packages/api/src/db.ts
@@ -15,6 +15,7 @@ export const initDb = () => {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
             waitForConnections: true,
             connectionLimit: 10,
             queueLimit: 0
